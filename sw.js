a3dbc2371cb8622872361f35cfc2c9e60c16a483
Md. Julfiker Hossen Shebbir <julfikerhossenshebbir@gmail.com>
2024-11-28 20:44:26 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "webgame-cache-v1";
+const urlsToCache = [
+  "/index.html",
+  "/stylesheetjhshebbir.css",
+  "/javajhshebbirscript.js",
+  ];
+
+// Install Service Worker and Cache Files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch Cached Files or Fallback to Network
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate Service Worker and Remove Old Caches
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
