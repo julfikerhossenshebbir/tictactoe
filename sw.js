9fe43e49559083873c5edf70f5d315c6272fd153
Md. Julfiker Hossen Shebbir <julfikerhossenshebbir@gmail.com>
2024-11-30 23:47:58 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,49 +1,37 @@
-// Cache version
-const CACHE_NAME = 'offline-pwa-v1';
-
-// Files to cache
-const FILES_TO_CACHE = [
-  '/',
-        '/index.html',  // আপনার HTML পেজ
-        '/stylesheetjhshebbir.css',  // আপনার CSS ফাইল
-        '/javajhshebbirscript.js',  // আপনার JavaScript ফাইল
-        '/sounds/sound-1.wav',  // শব্দ ফাইল
-        '/sounds/sound-2.wav',  // শব্দ ফাইল
-        '/sounds/sound-3.wav',  // শব্দ ফাইল
-        '/sounds/sound-4.wav',  // শব্দ ফাইল
+const CACHE_NAME = "pwa-cache-v1";
+const CACHE_ASSETS = [
+  "./",
+  "./index.html",
+  "./stylesheetjhshebbir.css",
+  "./javajhshebbirscript.js",
+  "./sounds/sound-1.wav",
+  "./sounds/sound-2.wav",
+  "./sounds/sound-3.wav",
+  "./sounds/sound-4.wav"
 ];
 
-// Install event
-self.addEventListener('install', event => {
+// Install Service Worker
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Caching files');
-      return cache.addAll(FILES_TO_CACHE);
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Caching files");
+      return cache.addAll(CACHE_ASSETS);
     })
   );
 });
 
-// Activate event
-self.addEventListener('activate', event => {
+// Activate Service Worker
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cache => {
+        cacheNames.map((cache) => {
           if (cache !== CACHE_NAME) {
-            console.log('Deleting old cache:', cache);
+            console.log("Clearing old cache");
             return caches.delete(cache);
           }
         })
       );
-    })
-  );
-});
-
-// Fetch event
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
     })
   );
 });
